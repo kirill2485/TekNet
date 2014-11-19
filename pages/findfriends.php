2f014f3e692200f9f83fa65fbc5a1fdc4cf7f415
kirill2485 <kirill2485@gmail.com>
2014-11-19 05:59:33 +0000

    FriendFinder updated

--- pages/findfriends.php
+++ pages/findfriends.php
@@ -1,6 +1,7 @@
 <?php
 include '../getelement.php';
 include '../includeme.php';
+include '../backendstuff/phplogin.php';
 ?>
 <html lang="en" class="no-js">
 	<head>
@@ -16,5 +17,18 @@ include '../includeme.php';
 				if ($email==null){
 				header( 'Location: index.php' ) ;
 				}
-				echo "Welcome to TekNet FriendFinder";
+				echo "<h1>Welcome to TekNet FriendFinder</h1>";
+				?>
+			</header>
+			<section class="color-2">
+				<?php
+				$result = $con->query("SELECT name FROM registeredusers");
+				$numrows=$result->num_rows;
+				echo $numrows , " users registered <br>";
+				$curnum=0;
+				while($numrows>$curnum){
+					$curnum= $curnum+1;
+					$listname=$result->fetch_row();
+					echo $listname[0], "<a href=http://www.theteknet.com/pages/userprofile.php?name=", $submit[0], "> Profile</a><br>";
+				}
 				?>
